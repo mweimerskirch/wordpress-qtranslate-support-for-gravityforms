f33b8a2a4b6a1caf0a70b627db9a9c6d3adabfc0
Michel Weimerskirch <michel@weimerskirch.net>
2015-02-20 21:01:37 +0100

    Refactoring + compatibility with qTranslate-X

--- qtranslate-support-for-gravityforms.php
+++ qtranslate-support-for-gravityforms.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 Plugin Name: qTranslate support for GravityForms
 Plugin URI: https://github.com/mweimerskirch/wordpress-qtranslate-support-for-gravityforms
@@ -8,98 +9,131 @@ Author: Michel Weimerskirch
 Author URI: http://michel.weimerskirch.net
 License: MIT
 */
-add_filter('gform_pre_render', 'qtranslate_gform_pre_render');
-add_filter('gform_pre_submission_filter', 'qtranslate_gform_pre_render');
-add_filter('gform_polls_form_pre_results', 'qtranslate_gform_pre_render');
-function qtranslate_gform_pre_render($form)
+
+
+class qTranslateSupportForGravityforms
 {
-	if (!function_exists('qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage')) return $form;
+	public function __construct()
+	{
+		add_filter('gform_pre_render', array($this, 'gform_pre_render'));
+		add_filter('gform_pre_submission_filter', array($this, 'gform_pre_render'));
+		add_filter('gform_polls_form_pre_results', array($this, 'gform_pre_render'));
 
-	if (isset($form['button']['text'])) {
-		$form['button']['text'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['button']['text']);
+		add_filter('gform_form_tag', array($this, 'gform_form_tag'));
+		add_filter("gform_confirmation", array($this, "gform_confirmation"), 10, 4);
+		add_filter("gform_pre_send_email", array($this, "gform_pre_send_email"));
 	}
-	if (isset($form['title'])) {
-		$form['title'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['title']);
-	}
-	if (isset($form['description'])) {
-		$form['description'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['description']);
-	}
-	if (isset($form['confirmation']['message'])) {
-		$form['confirmation']['message'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['confirmation']['message']);
-	}
-	if (isset($form['fields'])) {
-		foreach ($form['fields'] as $id => $field) {
-			$form['fields'][$id]['label'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['fields'][$id]['label']);
-			$form['fields'][$id]['content'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['fields'][$id]['content']);
-			$form['fields'][$id]['description'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['fields'][$id]['description']);
-			$form['fields'][$id]['defaultValue'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['fields'][$id]['defaultValue']);
-			$form['fields'][$id]['errorMessage'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['fields'][$id]['errorMessage']);
-			$form['fields'][$id]['validation_message'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['fields'][$id]['validation_message']);
-			$form['fields'][$id]['choices'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['fields'][$id]['choices']);
 
-			if (isset($form['fields'][$id]['conditionalLogic']['rules'])) {
-				foreach ($form['fields'][$id]['conditionalLogic']['rules'] as $value => $key) {
-					foreach ($key as $value2 => $key2) {
-						$form['fields'][$id]['conditionalLogic']['rules'][$value][$value2] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($key2);
-					}
-				}
-			}
+	private function isEnabled()
+	{
+		return (function_exists('qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage') || function_exists('qtranxf_useCurrentLanguageIfNotFoundUseDefaultLanguage'));
+	}
 
-			// Support for the poll add-on
-			if (isset($form['fields'][$id]['choices'])) {
-				foreach ($form['fields'][$id]['choices'] as $value => $key) {
-					$form['fields'][$id]['choices'][$value]['text'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($key['text']);
-				}
-			}
-                        
-                        $form['fields'][$id]['nextButton']['text'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['fields'][$id]['nextButton']['text']);
-                        $form['fields'][$id]['previousButton']['text'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['fields'][$id]['previousButton']['text']);
-
+	private function translate($text)
+	{
+		if (function_exists('qtranxf_useCurrentLanguageIfNotFoundUseDefaultLanguage')) {
+			return qtranxf_useCurrentLanguageIfNotFoundUseDefaultLanguage($text);
+		} else {
+			return qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($text);
 		}
 	}
-        if (isset($form['lastPageButton'])) {
-		$form['lastPageButton'] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['lastPageButton']);
+
+	private function convertURL($url, $lang)
+	{
+		if (function_exists('qtranxf_convertURL')) {
+			return qtranxf_convertURL($url, $lang);
+		} else {
+			return qtrans_convertURL($url, $lang);
+		}
 	}
-        if(isset($form['pagination'])){
-            foreach ($form['pagination']['pages'] as $id => $title){
-                $form['pagination']['pages'][$id] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($form['pagination']['pages'][$id]);
-            }
-        }
-	return $form;
+
+	public function gform_pre_render($form)
+	{
+		if (!$this->isEnabled()) return $form;
+
+		if (isset($form['button']['text'])) {
+			$form['button']['text'] = $this->translate($form['button']['text']);
+		}
+		if (isset($form['title'])) {
+			$form['title'] = $this->translate($form['title']);
+		}
+		if (isset($form['description'])) {
+			$form['description'] = $this->translate($form['description']);
+		}
+		if (isset($form['confirmation']['message'])) {
+			$form['confirmation']['message'] = $this->translate($form['confirmation']['message']);
+		}
+		if (isset($form['fields'])) {
+			foreach ($form['fields'] as $id => $field) {
+				$form['fields'][$id]['label'] = $this->translate($form['fields'][$id]['label']);
+				$form['fields'][$id]['content'] = $this->translate($form['fields'][$id]['content']);
+				$form['fields'][$id]['description'] = $this->translate($form['fields'][$id]['description']);
+				$form['fields'][$id]['defaultValue'] = $this->translate($form['fields'][$id]['defaultValue']);
+				$form['fields'][$id]['errorMessage'] = $this->translate($form['fields'][$id]['errorMessage']);
+				$form['fields'][$id]['validation_message'] = $this->translate($form['fields'][$id]['validation_message']);
+				$form['fields'][$id]['choices'] = $this->translate($form['fields'][$id]['choices']);
+
+				if (isset($form['fields'][$id]['conditionalLogic']['rules'])) {
+					foreach ($form['fields'][$id]['conditionalLogic']['rules'] as $value => $key) {
+						foreach ($key as $value2 => $key2) {
+							$form['fields'][$id]['conditionalLogic']['rules'][$value][$value2] = $this->translate($key2);
+						}
+					}
+				}
+
+				// Support for the poll add-on
+				if (isset($form['fields'][$id]['choices'])) {
+					foreach ($form['fields'][$id]['choices'] as $value => $key) {
+						$form['fields'][$id]['choices'][$value]['text'] = $this->translate($key['text']);
+					}
+				}
+				$form['fields'][$id]['nextButton']['text'] = $this->translate($form['fields'][$id]['nextButton']['text']);
+				$form['fields'][$id]['previousButton']['text'] = $this->translate($form['fields'][$id]['previousButton']['text']);
+
+			}
+		}
+		if (isset($form['lastPageButton'])) {
+			$form['lastPageButton'] = $this->translate($form['lastPageButton']);
+		}
+		if (isset($form['pagination'])) {
+			foreach ($form['pagination']['pages'] as $id => $title) {
+				$form['pagination']['pages'][$id] = $this->translate($form['pagination']['pages'][$id]);
+			}
+		}
+		return $form;
+	}
+
+	public function gform_form_action_attribute($matches)
+	{
+		global $q_config;
+		return 'action="' . $this->convertURL($matches[1], $q_config['language']) . '"';
+	}
+
+	public function gform_form_tag($tag)
+	{
+		if (!$this->isEnabled()) return $tag;
+
+		$tag = preg_replace_callback("|action='([^']+)'|", array(&$this, 'gform_form_action_attribute'), $tag);
+		return $tag;
+	}
+
+	public function gform_confirmation($confirmation, $form, $lead, $ajax)
+	{
+		if (!$this->isEnabled()) return $confirmation;
+
+		$confirmation = $this->translate($confirmation);
+		return $confirmation;
+	}
+
+	public function gform_pre_send_email($email)
+	{
+		if (!$this->isEnabled()) return $email;
+
+		$email["message"] = $this->translate($email["message"]);
+		$email["subject"] = $this->translate($email["subject"]);
+		return $email;
+	}
+
 }
 
-add_filter('gform_form_tag', 'qtranslate_gform_form_tag');
-function qtranslate_gform_form_tag($tag)
-{
-	if (!function_exists('qtrans_convertURL')) return $tag;
-
-	global $q_config;
-	$tag = preg_replace_callback(
-		"|action='([^']+)'|",
-		create_function(
-			'$matches',
-			'return "action=\'" . qtrans_convertURL($matches[1], ' . $q_config['language'] . ') . "\'";'
-		),
-		$tag
-	);
-	return $tag;
-}
-
-add_filter("gform_confirmation", "qtranslate_gform_confirmation", 10, 4);
-function qtranslate_gform_confirmation($confirmation, $form, $lead, $ajax)
-{
-	if (!function_exists('qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage')) return $confirmation;
-
-	$confirmation = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($confirmation);
-	return $confirmation;
-}
-
-add_filter("gform_pre_send_email", "qtranslate_gform_pre_send_email");
-function qtranslate_gform_pre_send_email($email)
-{
-	if (!function_exists('qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage')) return $email;
-
-	$email["message"] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($email["message"]);
-	$email["subject"] = qtrans_useCurrentLanguageIfNotFoundUseDefaultLanguage($email["subject"]);
-	return $email;
-}
+new qTranslateSupportForGravityforms();
