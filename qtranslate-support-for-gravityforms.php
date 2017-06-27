3454a57808ba9baa0b715fd2ff8b95c681d418cb
Michel Weimerskirch <michel@weimerskirch.net>
2017-06-27 11:06:46 +0200

    Translate placeholders in sub-inputs

--- qtranslate-support-for-gravityforms.php
+++ qtranslate-support-for-gravityforms.php
@@ -88,6 +88,9 @@ class qTranslateSupportForGravityforms
 						if(isset($input['customLabel'])) {
 							$form['fields'][$id]->inputs[$input_id]['customLabel'] = $this->translate($input['customLabel']);
 						}
+						if(isset($input['placeholder'])) {
+							$form['fields'][$id]->inputs[$input_id]['placeholder'] = $this->translate($input['placeholder']);
+						}
 					}
 				}
 
